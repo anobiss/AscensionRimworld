f4de9ba55647cfc6ba7742c2555cfb643408f4eb
anobiss <164560412+anobiss@users.noreply.github.com>
2024-07-13 19:34:09 -0700

    0.12.1

---
+++ 1.5/Source/Ascension/GCBreakthrough_Hediff.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Verse;
+
+namespace Ascension
+{
+    public class GCBreakthrough_Hediff : HediffWithComps
+    {
+        public override void PostTick()
+        {
+            base.PostTick();
+            //optimize later
+            if (pawn.CurJobDef != AscensionDefOf.AS_GoldenCoreBreakthrough)
+            {
+                Severity = 0;
+            }
+        }
+    }
+}
